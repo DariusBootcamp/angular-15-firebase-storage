fd4678533572f7f487260dcb60eb37c42d7dc75c
Darius Dang <dariusbc2023@gmail.com>
2023-04-05 14:20:03 +0700

    angular-firestorage-service

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBaqWYI6jyHoQhjxcfvieLVOp0s8UerqOo',
+    authDomain: 'angular-fire-2ae38.firebaseapp.com',
+    // databaseURL: 'https://bezkoder-firebase.firebaseio.com',
+    projectId: 'angular-fire-2ae38',
+    storageBucket: 'angular-fire-2ae38.appspot.com',
+    messagingSenderId: '148047679067',
+    appId: '1:148047679067:web:3e97fffacefe110158f97e'
+  }
+};
